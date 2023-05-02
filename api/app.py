e4123793972441d54a710a65072d2dac5bbc6eb5
asari <32879431+deomorxsy@users.noreply.github.com>
2023-05-02 14:31:09 -0300

    feat: add POST method on route

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 import json
@@ -20,15 +20,31 @@ def create_app():
     def ping_pong():
         return jsonify('pong o/')
 
-    @app.route('/books', methods=['GET'])
+    @app.route('/books', methods=['GET', 'POST'])
     def all_books():
-        return jsonify({
-            'status': 'success',
-            'books': my_dict['books']
+        #response object, first item status
+        rsp_obj = {'status': 'success'}
+
+        if (request.method == 'POST'):
+            post_data = request.get_json()
+            # Since dict has no attribute 'append',
+            # you just have to search for a list/array
+            # -like structure that can be appended. It
+            # would be ["books"].
+            my_dict['books'].append({
+                    'title': post_data.get('title'),
+                    'author': post_data.get('author'),
+                    'read': post_data.get('read')
+            })
+            rsp_obj['message'] = 'you just added a book.'
+        else:
+            # if the request is any other different from post,
+            #rsp_obj['books'] = my_dict
+            rsp_obj = my_dict
+        return jsonify(rsp_obj)
             # done using the leftmost key because doing so I
             # do not need to change neither the code key
             # variable nor the JSON file.
-        })
 
     return app
 
